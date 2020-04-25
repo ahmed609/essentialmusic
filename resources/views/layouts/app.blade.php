9927697f6a45aeaad596516a2ac05d568e00bb60
Ahmed Davis <adavis@educationalresource.com>
2020-04-25 05:35:37 -0400

    Update app.blade.php
    Included script that instanciated tinymce into the template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,19 @@
             @include('inc.messages')
             @yield('content')
         </div>
+        
         <script src="{{ asset('/js/app.js') }}"></script>
+        
+        {{-- Tiny MCE --}}
+        <script>
+            tinymce.init({
+              selector: 'textarea',
+              plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+              toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+              toolbar_mode: 'floating',
+              tinycomments_mode: 'embedded',
+              tinycomments_author: 'Author name',
+            });
+          </script>
     </body>
 </html>
