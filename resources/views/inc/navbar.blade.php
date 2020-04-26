71449f2e0733b254268ea2af840c55c79b4a90a6
Ahmed Davis <adavis@educationalresource.com>
2020-04-26 18:11:18 -0400

    Update navbar.blade.php
    Made code more readable

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,18 +6,10 @@
   
     <div class="collapse navbar-collapse" id="navbarsCollapse">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-            <a class="nav-link active" href="/">Home</a> 
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/services">Services</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/posts">Blog</a>
-        </li>
+        <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="/services">Services</a></li>
+        <li class="nav-item"><a class="nav-link" href="/posts">Blog</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
           <li class="nav-item"><a class="nav-link" href="/posts/create">Create Post</a></li>
