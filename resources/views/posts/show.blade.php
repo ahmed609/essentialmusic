dd5e9510fcfd5a84a392af5dac55803584005e57
Ahmed Davis <adavis@educationalresource.com>
2020-04-26 20:06:02 -0400

    Update show.blade.php
    Implemented button for deleting posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,5 +10,10 @@
     <small>Written on {{$post->created_at}}</small>
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+
+    {!! Form::open(['action'=> ['PostsController@destroy', $post->id], 'method' => 'POST', 'class'=>'pull-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('DELETE', ['class'=> 'btn btn-danger'])}}
+    {!! Form::close() !!}
 @endsection
 
